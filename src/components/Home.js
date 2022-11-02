8417f14983e1095ee03a572b2b4eaafc43b6f752
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-02 21:00:47 +0600

    Raad data show success fully

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const users = useLoaderData();
     return (
         <div>
-            <h1>This is home</h1>
+            <h1>Total users: {users.length}</h1>
+
+
+            <div>
+                {
+                    users.map(user => <p key={user._id}>
+                        {user.name}, {user.email}
+                    </p>)
+                }
+            </div>
+
         </div>
+        
     );
 };
 
